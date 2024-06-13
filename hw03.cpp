74dbb7aa3d0cd8daacf6d693899208e4c746a90b
若水 <168269104+Crystal0716@users.noreply.github.com>
2024-06-13 21:02:52 +0800

    Add files via upload

---
+++ hw03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+	int a,b,c,d ;
+	cout << "Enter integer 1 : " ;
+	cin >> a ;
+	cout << "Enter integer 2 : " ;
+	cin >> b ;
+	cout << "Enter integer 3 : " ;
+	cin >> c ;
+	d=a*b*c ;
+	cout << a << " * " << b << " * " << c << " = " << d ;
+
+	return (0);
+}
